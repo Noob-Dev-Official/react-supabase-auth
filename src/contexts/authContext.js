7a3ff716e6652117e820dd1198d69932a0447ed0
Muntasir2001 <muntasirenamalhuda@gmail.com>
2022-01-18 16:31:28 +0000

    completed authContext, not tested yet

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -4,43 +4,82 @@ import { createClient } from '@supabase/supabase-js';
 const AuthContext = createContext();
 
 export const useAuth = () => {
-	return useContext(AuthContext);
+   return useContext(AuthContext);
 };
 
 export const AuthProvider = ({ children }) => {
-	const supabaseUrl = process.env.SUPABASE_URL;
-	const supabaseKey = process.env.SUPABASE_KEY;
-	const supabase = createClient(supabaseUrl, supabaseKey);
-	const { auth } = supabase;
+   const supabaseUrl = process.env.SUPABASE_URL;
+   const supabaseKey = process.env.SUPABASE_KEY;
+   const supabase = createClient(supabaseUrl, supabaseKey);
+   const { auth } = supabase;
 
-	const [currentUser, setCurrentUser] = useState();
-	const [loading, setLoading] = useState(true);
+   const [currentUser, setCurrentUser] = useState();
+   const [loading, setLoading] = useState(true);
 
-	const currentUser = () => {
-		return auth.user();
-	};
+   const currentUser = () => {
+      return auth.user();
+   };
 
-	const signup = (email, password) => {
-		return auth.signUp({ email: email, password: password });
-	};
+   const signup = (email, password) => {
+      return auth.signUp({ email: email, password: password });
+   };
 
-	const signin = (email, password) => {
-		return auth.signIn({ email: email, password: password });
-	};
+   const signin = (email, password) => {
+      return auth.signIn({ email: email, password: password });
+   };
 
-	const signout = () => {
-		return auth.signOut();
-	};
+   const signout = () => {
+      return auth.signOut();
+   };
 
-	const resetPassword = (email) => {
-		return auth.api.resetPasswordForEmail(email);
-	};
+   const resetPassword = (email) => {
+      return auth.api.resetPasswordForEmail(email);
+   };
 
-	const updateEmail = (email) => {
-		return auth.update({ email: email });
-	};
+   const updateEmail = (email) => {
+      return auth.update({ email: email });
+   };
 
-	const updatePassword = (password) => {
-		return auth.update({ password: password });
-	};
+   const updatePassword = (password) => {
+      return auth.update({ password: password });
+   };
+
+   const initialiseUser = () => {
+      const user = auth.user();
+      let username;
+
+      if (user) {
+         username = user.user_metadata.user_name;
+      }
+
+      setCurrentUser(user);
+      localStorage.setItem('username', username);
+   };
+
+   const value = {
+      currentUser,
+      signup,
+      signin,
+      signout,
+      resetPassword,
+      updateEmail,
+      updatePassword,
+   };
+
+   useEffect(() => {
+      const unsubscribe = auth.onAuthStateChange((event, session) => {
+         console.log(event, session);
+
+         if (event === 'SIGNED_IN') initialiseUser;
+         setLoading(false);
+      });
+
+      return unsubscribe;
+   }, []);
+
+   return (
+      <AuthContext.Provider value={value}>
+         {!loading && children}
+      </AuthContext.Provider>
+   );
 };
