bce956158f5667013c8361773736c7b5a3a62405
Muntasir <muntasirenamalhuda@gmail.com>
2022-01-23 18:48:41 +0000

    updated AuthContext file name spelling

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -4,89 +4,89 @@ import { createClient } from '@supabase/supabase-js';
 const AuthContext = createContext({});
 
 export const useAuth = () => {
-   return useContext(AuthContext);
+	return useContext(AuthContext);
 };
 
 export const AuthProvider = ({ children }) => {
-   const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-   const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-   const supabase = createClient(supabaseUrl, supabaseKey);
-   const { auth } = supabase;
+	const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+	const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+	const supabase = createClient(supabaseUrl, supabaseKey);
+	const { auth } = supabase;
 
-   const [currentUser, setCurrentUser] = useState();
-   const [isSignedIn, setIsSignedIn] = useState(false);
-   const [loading, setLoading] = useState(true);
+	const [currentUser, setCurrentUser] = useState();
+	const [isSignedIn, setIsSignedIn] = useState(false);
+	const [loading, setLoading] = useState(true);
 
-   const signup = (email, password) => {
-      return auth.signUp({ email: email, password: password });
-   };
+	const signup = (email, password) => {
+		return auth.signUp({ email: email, password: password });
+	};
 
-   const signin = (email, password) => {
-      return auth.signIn({ email: email, password: password });
-   };
+	const signin = (email, password) => {
+		return auth.signIn({ email: email, password: password });
+	};
 
-   const signout = () => {
-      return auth.signOut();
-   };
+	const signout = () => {
+		return auth.signOut();
+	};
 
-   const resetPassword = (email) => {
-      return auth.api.resetPasswordForEmail(email);
-   };
+	const resetPassword = (email) => {
+		return auth.api.resetPasswordForEmail(email);
+	};
 
-   const updateEmail = (email) => {
-      return auth.update({ email: email });
-   };
+	const updateEmail = (email) => {
+		return auth.update({ email: email });
+	};
 
-   const updatePassword = (password) => {
-      return auth.update({ password: password });
-   };
+	const updatePassword = (password) => {
+		return auth.update({ password: password });
+	};
 
-   const user = () => {
-      return auth.user();
-   };
+	const user = () => {
+		return auth.user();
+	};
 
-   const initialiseUser = () => {
-      const user = auth.user();
-      let username;
+	const initialiseUser = () => {
+		const user = auth.user();
+		let username;
 
-      if (user) {
-         username = user.user_metadata.user_name;
-      }
+		if (user) {
+			username = user.user_metadata.user_name;
+		}
 
-      setCurrentUser(user);
-      localStorage.setItem('username', username);
-   };
+		setCurrentUser(user);
+		localStorage.setItem('username', username);
+	};
 
-   const value = {
-      isSignedIn,
-      currentUser,
-      user,
-      signup,
-      signin,
-      signout,
-      resetPassword,
-      updateEmail,
-      updatePassword,
-   };
+	const value = {
+		isSignedIn,
+		currentUser,
+		user,
+		signup,
+		signin,
+		signout,
+		resetPassword,
+		updateEmail,
+		updatePassword,
+	};
 
-   useEffect(() => {
-      const unsubscribe = auth.onAuthStateChange((event, session) => {
-         console.log(event, session);
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChange((event, session) => {
+			console.log(event, session);
 
-         if (event === 'SIGNED_IN') {
-            initialiseUser();
-            setIsSignedIn(false);
-         }
-      });
+			if (event === 'SIGNED_IN') {
+				initialiseUser();
+				setIsSignedIn(false);
+			}
+		});
 
-      setLoading(false);
-      return unsubscribe;
-      // eslint-disable-next-line
-   }, []);
+		setLoading(false);
+		return unsubscribe;
+		// eslint-disable-next-line
+	}, []);
 
-   return (
-      <AuthContext.Provider value={value}>
-         {!loading && children}
-      </AuthContext.Provider>
-   );
+	return (
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	);
 };
--- src/pages/UpdateProfile.js
+++ src/pages/UpdateProfile.js
@@ -5,148 +5,148 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import Alert from '../components/Alert';
 import {
-   AuthFormParent,
-   AuthFormHeading,
-   AuthForm,
-   AuthFormLabel,
-   AuthFormInput,
-   AuthFormEmailDiv,
-   AuthFormPasswordDiv,
-   AuthFormSubmitBtn,
-   AuthFormBottomText,
+	AuthFormParent,
+	AuthFormHeading,
+	AuthForm,
+	AuthFormLabel,
+	AuthFormInput,
+	AuthFormEmailDiv,
+	AuthFormPasswordDiv,
+	AuthFormSubmitBtn,
+	AuthFormBottomText,
 } from '../components/AuthFormComponents';
 
 const UpdateProfile = () => {
-   const [email, setEmail] = useState({ email: '' });
-   const [password, setPassword] = useState({ password: '' });
-   const [error, setError] = useState(false);
-   const [errorMssg, setErrorMssg] = useState('');
-   const [loading, setLoading] = useState(false);
+	const [email, setEmail] = useState({ email: '' });
+	const [password, setPassword] = useState({ password: '' });
+	const [error, setError] = useState(false);
+	const [errorMssg, setErrorMssg] = useState('');
+	const [loading, setLoading] = useState(false);
 
-   const { currentUser, updateEmail, updatePassword } = useAuth();
+	const { currentUser, updateEmail, updatePassword } = useAuth();
 
-   const navigate = useNavigate();
+	const navigate = useNavigate();
 
-   const confirmPasswordRef = useRef();
+	const confirmPasswordRef = useRef();
 
-   const onEmailChange = (e) => {
-      setEmail(() => {
-         return {
-            [e.target.name]: e.target.value,
-         };
-      });
-   };
+	const onEmailChange = (e) => {
+		setEmail(() => {
+			return {
+				[e.target.name]: e.target.value,
+			};
+		});
+	};
 
-   const onPasswordChange = (e) => {
-      setPassword(() => {
-         return {
-            [e.target.name]: e.target.value,
-         };
-      });
-   };
+	const onPasswordChange = (e) => {
+		setPassword(() => {
+			return {
+				[e.target.name]: e.target.value,
+			};
+		});
+	};
 
-   const onFormSubmit = (e) => {
-      e.preventDefault();
+	const onFormSubmit = (e) => {
+		e.preventDefault();
 
-      if (confirmPasswordRef.current.value !== password.password) {
-         setPassword({ password: '' });
-         confirmPasswordRef.current.value = '';
+		if (confirmPasswordRef.current.value !== password.password) {
+			setPassword({ password: '' });
+			confirmPasswordRef.current.value = '';
 
-         setError(true);
-         setErrorMssg('Passwords do not match!');
+			setError(true);
+			setErrorMssg('Passwords do not match!');
 
-         hideErrorMssg();
-         return 'incorrect password';
-      } else {
-         const promises = [];
-         setLoading(true);
-         setError(false);
+			hideErrorMssg();
+			return 'incorrect password';
+		} else {
+			const promises = [];
+			setLoading(true);
+			setError(false);
 
-         if (email.email && email.email !== currentUser.email) {
-            promises.push(updateEmail(email.email));
-         }
+			if (email.email && email.email !== currentUser.email) {
+				promises.push(updateEmail(email.email));
+			}
 
-         if (password.password) {
-            promises.push(updatePassword(password.password));
-         }
+			if (password.password) {
+				promises.push(updatePassword(password.password));
+			}
 
-         Promise.all(promises)
-            .then(() => {
-               navigate('/');
-            })
-            .catch((err) => {
-               setErrorMssg(err);
-               console.log(err);
-               hideErrorMssg();
-            })
-            .finally(() => {
-               setLoading(false);
-            });
-      }
+			Promise.all(promises)
+				.then(() => {
+					navigate('/');
+				})
+				.catch((err) => {
+					setErrorMssg(err);
+					console.log(err);
+					hideErrorMssg();
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+		}
 
-      setLoading(false);
-   };
+		setLoading(false);
+	};
 
-   const hideErrorMssg = () => {
-      setTimeout(() => {
-         setError(false);
-         setErrorMssg('');
-      }, 5000);
-   };
+	const hideErrorMssg = () => {
+		setTimeout(() => {
+			setError(false);
+			setErrorMssg('');
+		}, 5000);
+	};
 
-   return (
-      <>
-         {error && <Alert mssg={errorMssg} />}
-         <AuthFormParent>
-            <AuthFormHeading>Update Profile</AuthFormHeading>
-            <AuthForm onSubmit={onFormSubmit}>
-               <AuthFormEmailDiv>
-                  <AuthFormLabel>Email</AuthFormLabel>
-                  <AuthFormInput
-                     type='text'
-                     name='email'
-                     value={email.email}
-                     onChange={onEmailChange}
-                  />
-               </AuthFormEmailDiv>
-               <AuthFormPasswordDiv>
-                  <AuthFormLabel>Password</AuthFormLabel>
-                  <AuthFormInput
-                     type='password'
-                     name='password'
-                     value={password.password}
-                     onChange={onPasswordChange}
-                  />
-               </AuthFormPasswordDiv>
-               <AuthFormPasswordDiv>
-                  <AuthFormLabel>Confirm Password</AuthFormLabel>
-                  <AuthFormInput
-                     ref={confirmPasswordRef}
-                     type='password'
-                     name='password'
-                     required
-                  />
-               </AuthFormPasswordDiv>
-               <AuthFormSubmitBtn
-                  disabled={loading}
-                  type='submit'
-                  name='submit'
-                  value='Update'
-               />
-            </AuthForm>
-         </AuthFormParent>
-         <AuthFormBottomText>
-            <Link
-               style={{
-                  color: '#D9004C',
-               }}
-               to='/'
-            >
-               Cancel
-            </Link>
-         </AuthFormBottomText>
-      </>
-   );
+	return (
+		<>
+			{error && <Alert mssg={errorMssg} />}
+			<AuthFormParent>
+				<AuthFormHeading>Update Profile</AuthFormHeading>
+				<AuthForm onSubmit={onFormSubmit}>
+					<AuthFormEmailDiv>
+						<AuthFormLabel>Email</AuthFormLabel>
+						<AuthFormInput
+							type='text'
+							name='email'
+							value={email.email}
+							onChange={onEmailChange}
+						/>
+					</AuthFormEmailDiv>
+					<AuthFormPasswordDiv>
+						<AuthFormLabel>Password</AuthFormLabel>
+						<AuthFormInput
+							type='password'
+							name='password'
+							value={password.password}
+							onChange={onPasswordChange}
+						/>
+					</AuthFormPasswordDiv>
+					<AuthFormPasswordDiv>
+						<AuthFormLabel>Confirm Password</AuthFormLabel>
+						<AuthFormInput
+							ref={confirmPasswordRef}
+							type='password'
+							name='password'
+							required
+						/>
+					</AuthFormPasswordDiv>
+					<AuthFormSubmitBtn
+						disabled={loading}
+						type='submit'
+						name='submit'
+						value='Update'
+					/>
+				</AuthForm>
+			</AuthFormParent>
+			<AuthFormBottomText>
+				<Link
+					style={{
+						color: '#D9004C',
+					}}
+					to='/'
+				>
+					Cancel
+				</Link>
+			</AuthFormBottomText>
+		</>
+	);
 };
 
 export default UpdateProfile;
