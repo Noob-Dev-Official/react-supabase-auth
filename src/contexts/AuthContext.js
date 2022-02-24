af09536d6cb25c2ac8195c00ba631e734cf29d1b
Muntasir2001 <muntasirenamalhuda@gmail.com>
2022-02-24 16:35:14 +0000

    no idea what i did

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -4,108 +4,106 @@ import { createClient } from '@supabase/supabase-js';
 const AuthContext = createContext({});
 
 export const useAuth = () => {
-	return useContext(AuthContext);
+   return useContext(AuthContext);
 };
 
 export const AuthProvider = ({ children }) => {
-	const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-	const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-	const supabase = createClient(supabaseUrl, supabaseKey);
-	const { auth } = supabase;
+   const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+   const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+   const supabase = createClient(supabaseUrl, supabaseKey);
+   const { auth } = supabase;
 
-	const [currentUser, setCurrentUser] = useState();
-	const [isSignedIn, setIsSignedIn] = useState(false);
-	const [loading, setLoading] = useState(true);
+   const [currentUser, setCurrentUser] = useState();
+   const [isSignedIn, setIsSignedIn] = useState(false);
+   const [loading, setLoading] = useState(true);
 
-	const signup = (email, password) => {
-		return auth.signUp({ email: email, password: password });
-	};
+   const signup = (email, password) => {
+      return auth.signUp({ email: email, password: password });
+   };
 
-	const signin = (email, password) => {
-		return auth.signIn({ email: email, password: password });
-	};
+   const signin = (email, password) => {
+      return auth.signIn({ email: email, password: password });
+   };
 
-	const signout = () => {
-		return auth.signOut();
-	};
+   const signout = () => {
+      return auth.signOut();
+   };
 
-	const resetPassword = (email) => {
-		return auth.api.resetPasswordForEmail(email);
-	};
+   const resetPassword = (email) => {
+      return auth.api.resetPasswordForEmail(email);
+   };
 
-	const updateEmail = (email) => {
-		return auth.update({ email: email });
-	};
+   const updateEmail = (email) => {
+      return auth.update({ email: email });
+   };
 
-	const updatePassword = (password) => {
-		return auth.update({ password: password });
-	};
+   const updatePassword = (password) => {
+      return auth.update({ password: password });
+   };
 
-	const user = () => {
-		return auth.user();
-	};
+   const user = () => {
+      return auth.user();
+   };
 
-	const initialiseUser = () => {
-		const user = auth.user();
-		let username;
+   const initialiseUser = () => {
+      const user = auth.user();
+      let username;
 
-		if (user) {
-			username = user.user_metadata.user_name;
-		}
+      if (user) {
+         username = user.user_metadata.user_name;
+      }
 
-		setCurrentUser(user);
-		localStorage.setItem('username', username);
-	};
+      setCurrentUser(user);
+      localStorage.setItem('username', username);
+   };
 
-	const value = {
-		isSignedIn,
-		currentUser,
-		user,
-		signup,
-		signin,
-		signout,
-		resetPassword,
-		updateEmail,
-		updatePassword,
-	};
+   const value = {
+      isSignedIn,
+      currentUser,
+      user,
+      signup,
+      signin,
+      signout,
+      resetPassword,
+      updateEmail,
+      updatePassword,
+   };
 
-	useEffect(() => {
-		auth.onAuthStateChange((event, session) => {
-			console.log('onAuthStateChange', { event, session });
+   useEffect(() => {
+      auth.onAuthStateChange((event, session) => {
+         console.log('onAuthStateChange', { event, session });
 
-			if (event === 'SIGNED_IN') {
-				initialiseUser();
-				setIsSignedIn(true);
-				console.log('im running');
-			}
-		});
+         if (event === 'SIGNED_IN') {
+            initialiseUser();
+            setIsSignedIn(true);
+            console.log('im running');
+         }
+      });
 
-		// const unsubscribe = auth.onAuthStateChange((event, session) => {
-		// 	console.log(event, session);
+      // const unsubscribe = auth.onAuthStateChange((event, session) => {
+      // 	console.log(event, session);
 
-		// 	if (event === 'SIGNED_IN') {
-		// 		initialiseUser();
-		// setIsSignedIn(true);
-		// console.log('im running');
-		// 	}
-		// });
+      // 	if (event === 'SIGNED_IN') {
+      // 		initialiseUser();
+      // setIsSignedIn(true);
+      // console.log('im running');
+      // 	}
+      // });
 
-		setLoading(false);
+      setLoading(false);
 
-		console.log(isSignedIn);
+      return () => {
+         supabase.removeSubscription();
+         console.log('Remove supabase subscription by useEffect unmount');
+      };
 
-		return () => {
-			supabase.removeSubscription();
-			console.log('Remove supabase subscription by useEffect unmount');
-		};
+      // return unsubscribe;
+      // eslint-disable-next-line
+   }, []);
 
-		// return unsubscribe;
-		// eslint-disable-next-line
-	}, []);
-
-	return (
-		<AuthContext.Provider value={value}>
-			{!loading && children}
-		</AuthContext.Provider>
-	);
+   return (
+      <AuthContext.Provider value={value}>
+         {!loading && children}
+      </AuthContext.Provider>
+   );
 };
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -5,113 +5,113 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import Alert from '../components/Alert';
 import {
-	AuthFormParent,
-	AuthFormHeading,
-	AuthForm,
-	AuthFormLabel,
-	AuthFormInput,
-	AuthFormEmailDiv,
-	AuthFormPasswordDiv,
-	AuthFormSubmitBtn,
-	AuthFormBottomText,
+   AuthFormParent,
+   AuthFormHeading,
+   AuthForm,
+   AuthFormLabel,
+   AuthFormInput,
+   AuthFormEmailDiv,
+   AuthFormPasswordDiv,
+   AuthFormSubmitBtn,
+   AuthFormBottomText,
 } from '../components/AuthFormComponents';
 
 const SignIn = () => {
-	const [email, setEmail] = useState({ email: '' });
-	const [password, setPassword] = useState({ password: '' });
-	const [error, setError] = useState(false);
-	const [errorMssg, setErrorMssg] = useState('');
-	const [loading, setLoading] = useState(false);
+   const [email, setEmail] = useState({ email: '' });
+   const [password, setPassword] = useState({ password: '' });
+   const [error, setError] = useState(false);
+   const [errorMssg, setErrorMssg] = useState('');
+   const [loading, setLoading] = useState(false);
 
-	const navigate = useNavigate();
+   const navigate = useNavigate();
 
-	const { signin } = useAuth();
+   const { signin } = useAuth();
 
-	const onEmailChange = (e) => {
-		setEmail(() => {
-			return {
-				[e.target.name]: e.target.value,
-			};
-		});
-	};
+   const onEmailChange = (e) => {
+      setEmail(() => {
+         return {
+            [e.target.name]: e.target.value,
+         };
+      });
+   };
 
-	const onPasswordChange = (e) => {
-		setPassword(() => {
-			return {
-				[e.target.name]: e.target.value,
-			};
-		});
-	};
+   const onPasswordChange = (e) => {
+      setPassword(() => {
+         return {
+            [e.target.name]: e.target.value,
+         };
+      });
+   };
 
-	//eslint-disable-next-line
-	const hideErrorMssg = () => {
-		setTimeout(() => {
-			setError(false);
-			setErrorMssg('');
-		}, 5000);
-	};
+   //eslint-disable-next-line
+   const hideErrorMssg = () => {
+      setTimeout(() => {
+         setError(false);
+         setErrorMssg('');
+      }, 5000);
+   };
 
-	const onFormSubmit = async (e) => {
-		e.preventDefault();
+   const onFormSubmit = async (e) => {
+      e.preventDefault();
 
-		try {
-			setError(false);
-			setLoading(true);
+      try {
+         setError(false);
+         // setLoading(true);
 
-			await signin(email.email, password.password);
-			navigate('/');
-		} catch (err) {
-			setErrorMssg('Cannot Sign In');
-			setError(true);
-			console.log(err);
-			// hideErrorMssg(); //this was the problem, need to fix it later - Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
-		}
+         await signin(email.email, password.password);
+         navigate('/');
+      } catch (err) {
+         setErrorMssg('Cannot Sign In');
+         setError(true);
+         console.log(err);
+         // hideErrorMssg(); //this was the problem, need to fix it later - Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+      }
 
-		setLoading(false);
-	};
+      // setLoading(false);
+   };
 
-	return (
-		<>
-			{error && <Alert mssg={errorMssg} />}
-			<AuthFormParent>
-				<AuthFormHeading>Sign In</AuthFormHeading>
-				<AuthForm onSubmit={onFormSubmit}>
-					<AuthFormEmailDiv>
-						<AuthFormLabel>Email</AuthFormLabel>
-						<AuthFormInput
-							type='text'
-							name='email'
-							value={email.email}
-							onChange={onEmailChange}
-							required
-						/>
-					</AuthFormEmailDiv>
-					<AuthFormPasswordDiv>
-						<AuthFormLabel>Password</AuthFormLabel>
-						<AuthFormInput
-							type='password'
-							name='password'
-							value={password.password}
-							onChange={onPasswordChange}
-							required
-						/>
-					</AuthFormPasswordDiv>
-					<AuthFormSubmitBtn
-						disabled={loading}
-						type='submit'
-						name='submit'
-						value='Sign In'
-					/>
-				</AuthForm>
-				<p>
-					Need an account?<Link to='/sign-up'> Sign Up</Link>
-				</p>
-			</AuthFormParent>
-			<AuthFormBottomText>
-				Forgot password?<Link to='/forgot-password'> Click Here</Link>
-			</AuthFormBottomText>
-		</>
-	);
+   return (
+      <>
+         {error && <Alert mssg={errorMssg} />}
+         <AuthFormParent>
+            <AuthFormHeading>Sign In</AuthFormHeading>
+            <AuthForm onSubmit={onFormSubmit}>
+               <AuthFormEmailDiv>
+                  <AuthFormLabel>Email</AuthFormLabel>
+                  <AuthFormInput
+                     type='text'
+                     name='email'
+                     value={email.email}
+                     onChange={onEmailChange}
+                     required
+                  />
+               </AuthFormEmailDiv>
+               <AuthFormPasswordDiv>
+                  <AuthFormLabel>Password</AuthFormLabel>
+                  <AuthFormInput
+                     type='password'
+                     name='password'
+                     value={password.password}
+                     onChange={onPasswordChange}
+                     required
+                  />
+               </AuthFormPasswordDiv>
+               <AuthFormSubmitBtn
+                  // disabled={loading}
+                  type='submit'
+                  name='submit'
+                  value='Sign In'
+               />
+            </AuthForm>
+            <p>
+               Need an account?<Link to='/sign-up'> Sign Up</Link>
+            </p>
+         </AuthFormParent>
+         <AuthFormBottomText>
+            Forgot password?<Link to='/forgot-password'> Click Here</Link>
+         </AuthFormBottomText>
+      </>
+   );
 };
 
 export default SignIn;
